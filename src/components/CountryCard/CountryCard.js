8f0428aadb98eaafa62663438b68a6bb0c61efb4
benni maxon <benni.maxon@gmail.com>
2023-01-18 15:17:45 -0800

    built CountryCard and Main components

---
+++ src/components/CountryCard/CountryCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CountryCard() {
+  return (
+    <div className="country-card">
+      <div className="name"></div>
+      <img className="flag"></img>
+    </div>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useCountries } from '../../hooks/countries.js';
+import CountryCard from '../CountryCard/CountryCard.js';
 
 export default function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      {useCountries().map((item) => (
+        <CountryCard key={item.id} {...item} />
+      ))}
+    </main>
+  );
 }
